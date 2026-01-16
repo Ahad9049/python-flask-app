2ab158edafa95995d88f7c0c340ec2acda9a59c2
abdulahad <abdulahad.devops@gmail.com>
2026-01-16 18:42:42 +0500

    update pyfile

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(message="Hello from Flask CI/CD App")
+    return jsonify(message="Hello from Abdul Ahad my Flask CI/CD App")
 
 @app.route("/health")
 def health():
